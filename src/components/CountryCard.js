e71cf9b10493acb816117e54b04f17f7333b4cb9
Shree Nandan Das <nayan@Shrees-MacBook-Air.local>
2025-03-06 15:31:33 +0530

    Test commit

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -4,7 +4,7 @@ import './CountryCard.css';
 function CountryCard({img, name}) {
   return (
     <div className="country-card">
-        <img src={img} alt={name} width={"100%"}/>
+        <img src={img} alt={name} width={"50%"}/>
         <h3>{name}</h3>
     </div>
   )
